0dd8fabe6ac76f75520cca449b02a2b11b4eff1c
Lyraedan <61066562+Lyraedan@users.noreply.github.com>
2021-05-11 00:31:58 +0100

    Character Controller

---
+++ Assets/Scripts/Entity/Player/CharacterController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float speed = 100.0f;
+    public Rigidbody body;
+
+    void Start()
+    {
+        // turn off the cursor
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void FixedUpdate()
+    {
+        float verticalMovement = Input.GetAxis("Vertical");
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        var move = (horizontalMovement * transform.right + verticalMovement * transform.forward).normalized;
+        body.velocity = move * speed * Time.deltaTime;
+
+        if (Input.GetKeyDown("escape"))
+        {
+            // turn on the cursor
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
