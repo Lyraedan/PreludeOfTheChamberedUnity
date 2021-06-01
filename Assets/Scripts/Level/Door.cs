012b77ed3f0e2e6ee1083bff9bfc28dd1551dbc7
Lyraedan <61066562+Lyraedan@users.noreply.github.com>
2021-06-01 08:37:30 +0100

    Door and Pressureplate scripts

---
+++ Assets/Scripts/Level/Door.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Door : MonoBehaviour
+{
+
+    public Transform openedPosition;
+    public Transform closedPosition;
+    public AudioClip openSfx;
+    public AudioClip closeSfx;
+    public bool opened = false;
+
+    private AudioSource src;
+
+    private void Start()
+    {
+        src = GetComponent<AudioSource>();
+    }
+
+    public void ToggleDoor()
+    {
+        if (opened) Close();
+        else Open();
+    }
+
+    public void Open()
+    {
+        src.clip = openSfx;
+        src.Play();
+        opened = true;
+    }
+
+    public void Close()
+    {
+        src.clip = closeSfx;
+        src.Play();
+        opened = false;
+    }
+
+}
---
+++ Assets/Scripts/Level/PressurePlate.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(AudioSource))]
+public class PressurePlate : MonoBehaviour
+{
+
+    public new Renderer renderer;
+    public List<Door> doors = new List<Door>();
+    [Header("Pressed")]
+    public AudioClip pressedSfx;
+    public Texture pressedTexture;
+    public UnityEvent onPressed;
+    [Header("Unpressed")]
+    public AudioClip unpressedSfx;
+    public Texture unPressedTexture
+    public UnityEvent onUnpressed;
+
+    private AudioSource src;
+
+    private void Start()
+    {
+        src = GetComponent<AudioSource>();
+    }
+
+    public void OnPressed()
+    {
+        renderer.material.mainTexture = pressedTexture;
+        src.clip = pressedSfx;
+        src.Play();
+        onPressed?.Invoke();
+    }
+
+    public void OnUnpressed()
+    {
+        renderer.material.mainTexture = unPressedTexture;
+        src.clip = unpressedSfx;
+        src.Play();
+        onUnpressed?.Invoke();
+    }
+}
