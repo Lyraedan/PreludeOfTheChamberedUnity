9fdb5dc839b37eeb8313850f9e8a072e14bc6476
Lyraedan <61066562+Lyraedan@users.noreply.github.com>
2021-06-07 21:00:56 +0100

    The camera will now tilt based on the direction you strafe in

--- Assets/Scripts/Entity/Player/MouseCameraLook.cs
+++ Assets/Scripts/Entity/Player/MouseCameraLook.cs
@@ -8,6 +8,8 @@ public class MouseCameraLook : MonoBehaviour
     public float sensitivity = 5.0f;
     [SerializeField]
     public float smoothing = 2.0f;
+    [SerializeField]
+    public float tiltStrengh = 3f;
     public GameObject character;
     private Vector2 mouseLook;
     private Vector2 smoothV;
@@ -16,8 +18,15 @@ public class MouseCameraLook : MonoBehaviour
     {
         bool doLock = Player.pauseGameplay || (Player.instance.isOnIce && Player.instance.body.velocity != Vector3.zero && !Player.pauseGameplay);
 
-        // Issue when frozen it glitches ouot todo fix
-        var md = !doLock ? new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) : Vector2.zero;
+        var mouseX = Input.GetAxisRaw("Mouse X");
+        var mouseY = Input.GetAxisRaw("Mouse Y");
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+        var md = !doLock ? new Vector2(mouseX, mouseY) : Vector2.zero;
+        var xy = !doLock ? new Vector2(horizontal, vertical) : Vector2.zero;
+
+        float rotZ = 0;
 
         // This makes it so we can at least stop mouse rotation
         if (!doLock)
@@ -26,13 +35,21 @@ public class MouseCameraLook : MonoBehaviour
             smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
             smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
             mouseLook += smoothV;
+
+            if(horizontal != 0)
+            {
+                rotZ = horizontal * tiltStrengh;
+            }
         }
 
+        //mouseLook.x += rotX;
         mouseLook.y = Mathf.Clamp(mouseLook.y, -90, 90);
         if (!doLock)
         {
             transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
             character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+
+            transform.localRotation = Quaternion.Euler(transform.localRotation.x, transform.localRotation.y, transform.localRotation.z + -rotZ);
         }
     }
 }
